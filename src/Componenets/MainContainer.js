4c92f81f60a2d0eabdddbd59fbf1b427e7c879e3
space console <spaceconsole@spaces-MacBook-Pro.local>
2024-08-05 21:26:19 +0530

    new

--- src/Componenets/MainContainer.js
+++ src/Componenets/MainContainer.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './Mycss.css';
 import SideBar from './SideBar';
 import WorkArea from './WorkArea';
+import ChatArea from './ChatArea';
 
 function MainContainer() {
     return (<div className='main-container'>
         <SideBar />
-        <WorkArea />
+        <ChatArea props= {{name: "Person1", timeStamp: "today"}} />
     </div>);
 }
 
--- src/Componenets/SideBar.js
+++ src/Componenets/SideBar.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Mycss.css'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 import GroupAddIcon from '@mui/icons-material/GroupAdd';
 import NightlightIcon from '@mui/icons-material/Nightlight';
 import SearchIcon from '@mui/icons-material/Search';
-import ChatItems from './ChatItems';
 import { IconButton } from '@mui/material';
+import ConversationItems from './ConversationItems';
 export default function SideBar() {
+  const [conversations, setConversations] = useState([
+    {
+      name: "Test1",
+      lastMessage: "Last Message #1",
+      timeStamp: "today",
+    },
+    {
+      name: "Test2",
+      lastMessage: "Last Message #2",
+      timeStamp: "today",
+    },
+    {
+      name: "Test3",
+      lastMessage: "Last Message #3",
+      timeStamp: "today",
+    },
+]);
   return (
     <div className='sidebar-container'>
       <div className = 'sb-header'>
@@ -34,8 +51,10 @@ export default function SideBar() {
         </IconButton>
         <input placeholder='search' className = 'search-box'></input>
       </div>
-      <div className = 'chats'> 
-      <ChatItems />
+      <div className = 'sb-conversationItems'> 
+        {conversations.map((conversation)=> {
+          return <ConversationItems props={conversation} key  = {conversation.name} />
+        })}
       </div>
     </div>
   )
